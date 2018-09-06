445aeaa9e6a62a8bd87a6d7933bbc3095a4ce957
Nitin Kr <nitin.nitp@gmail.com>
2018-09-06 15:58:13 +0530

    error message fix with salt 3.x

--- salt/modules/zpool.py
+++ salt/modules/zpool.py
@@ -31,7 +31,7 @@ def __virtual__():
     '''
     Only load when the platform has zfs support
     '''
-    if __grains__['zfs_support']:
+    if __grains__.get('zfs_support'):
         return __virtualname__
     else:
         return (False, "The zpool module cannot be loaded: zfs not supported")
